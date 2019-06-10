df45ebf12b556f8a1013fdfb4e498b14441ad135
xinghunm <xinghunm@curacloudcorp.com>
2019-06-10 16:54:57 +0800

    enable to generate the question detail

--- utils/generate-question.js
+++ utils/generate-question.js
@@ -1,14 +1,13 @@
 import forEach from "lodash/forEach";
-import { walkSource, getArticles, generateHtmls } from "./helper";
+import { walkSource, getArticles, generateQuestions } from "./helper";
 
 const startGenerate = () => {
   const baseDir = `./source/questions`;
-  const dist = "./db/answers";
   const files = walkSource(baseDir);
   const articles = getArticles(files);
-  const qustions = generateHtmls(dist, articles);
+  const qustions = generateQuestions(articles);
   forEach(qustions, ({ title }) => {
-    console.log(`Generated: ${dist}/${title}.html`);
+    console.log(`Generated: ${title}.html`);
   });
   console.log(`${qustions.length} files generated`);
 };
--- utils/helper.js
+++ utils/helper.js
@@ -25,14 +25,22 @@ const walkSource = (path) => {
 };
 
 const getArticleInfo = (articleStr) => {
-  const match = articleStr.match(/(---\n[\S\s]*)\n---([\S\s]*)/);
+  const parts = articleStr.split("---");
   let info;
   try {
-    if (match && match.length >= 3) {
-      const head = match[1];
-      const headInfo = yml.safeLoad(head);
-      const content = match[2];
-      info = { headInfo, content };
+    if (parts && parts.length >= 3) {
+      if (parts.length === 3) {
+        const head = parts[1];
+        const headInfo = yml.safeLoad(head);
+        const answer = parts[2];
+        info = { headInfo, answer };
+      } else if (parts.length === 4) {
+        const head = parts[1];
+        const headInfo = yml.safeLoad(head);
+        const questionDetail = parts[2];
+        const answer = parts[3];
+        info = { headInfo, answer, questionDetail };
+      }
     }
   } catch (e) {
     throw e;
@@ -60,14 +68,14 @@ const getArticles = (files) => {
   return articles;
 };
 
-const updateDBCreator = db => (article) => {
-  const { headInfo, content } = article;
+const updateQustionCreator = db => (article) => {
+  const { headInfo, answer } = article;
   let questionInfo = { ...qustionSchema, ...headInfo };
   const questions = db.get("questions").value();
   const byId = questions.byId || {};
   const allIds = questions.allIds || [];
   const found = find(byId, ({ title }) => title === headInfo.title);
-  const hash = md5.hash(content);
+  const hash = md5.hash(answer);
   if (found) {
     if (hash !== found.md5) {
       questionInfo = { ...found, ...headInfo, md5: hash };
@@ -85,33 +93,35 @@ const updateDBCreator = db => (article) => {
   return questionInfo;
 };
 
-const generateHtmls = (dist, articles) => {
+const generateQuestions = (articles) => {
   try {
     const adapter = new FileSync("db/db.json");
     const db = lowdb(adapter);
-    const updateDB = updateDBCreator(db);
-
+    const updateQustion = updateQustionCreator(db);
+    const qustionsDir = "./db/questions";
+    const answersDir = "./db/answers";
     const qustions = [];
-    if (!fs.existsSync(dist)) {
-      fs.mkdirSync(dist);
-    }
 
     for (let i = 0; i < articles.length; i++) {
       const article = articles[i];
-      const questionInfo = updateDB(article);
+      const questionInfo = updateQustion(article);
       if (questionInfo) {
-        const { content } = article;
-        const html = marked(content);
-        const fileName = `${dist}/${questionInfo.id}.html`;
+        const { answer, questionDetail } = article;
+        const html = marked(answer);
+        const fileName = `${answersDir}/${questionInfo.id}.html`;
         fs.writeFileSync(fileName, html, "utf8");
+        if (questionDetail) {
+          const questionName = `${qustionsDir}/${questionInfo.id}.html`;
+          fs.writeFileSync(questionName, html, "utf8");
+        }
         qustions.push(questionInfo);
       }
     }
     return qustions;
   } catch (e) {
-    console.log("generateHtmls error", e);
+    console.log("generateQuestions error", e);
     throw e;
   }
 };
 
-export { walkSource, getArticles, generateHtmls };
+export { walkSource, getArticles, generateQuestions };
