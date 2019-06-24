74390fb85b5431734252e50afef115bbcb2610a2
xinghunm <xinghunm@curacloudcorp.com>
2019-06-24 14:16:45 +0800

    tmp

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -25,10 +25,23 @@ log(){
   echo "$DATE ${SCRIPT_NAME} : ${LOG_INFO}" >> ${SHELL_LOG}
 }
 
-fetchDBJson(){
-  echo "Fetch db.json"
-  log "Fetch db.json"
-  ssh ${REMOTE} "cd /root/project/front-end-ship-server && git add db/db.json && git commit -m "update db.json""
+pushDBJsonOnRemote(){
+  echo "Push db.json on remote..."
+  log "Push db.json on remote"
+  result=`ssh ${REMOTE} "cd /root/project/front-end-ship-server && git add . && git commit -m 'update db.json' && git push origin master"`
+  echo $result
+}
+
+pushDBJsonOnLocal(){
+  echo "Push db.json on local..."
+  log "Push db.json on local"
+  git push origin master || exit 1
+}
+
+updateDBJson(){
+  echo "Update db.json..."
+  log "Update db.json"
+  git pull --rebase || exit 1
 }
 
 deploy(){
@@ -37,19 +50,27 @@ deploy(){
   ssh ${REMOTE} "cd /root/project/front-end-ship-server && git pull && pm2 stop all && yarn build && pm2 start server.js"
 }
 
+function printRed(){
+  echo -e "\033[31m\033[01m[ $1 ]\033[0m"
+}
+
 main(){
+  #check files to be commited
   result=`git status --porcelain`
   if [[ $result ]];then
-     echo "files need to commit" && exit
+     printRed "There are files need to commit" && exit
   fi
-  fetchDBJson
-  # if [ -f "$LOCK_FILE" ];then
-  #    log "${SCRIPT_NAME} is running"
-  #    echo "${SCRIPT_NAME} is running" && exit
-  # fi
-  # lock
-  # deploy
-  # unlock
+
+  if [ -f "$LOCK_FILE" ];then
+     log "${SCRIPT_NAME} is running"
+     printRed "${SCRIPT_NAME} is running" && exit
+  fi
+  lock
+  pushDBJsonOnRemote
+  updateDBJson
+  pushDBJsonOnLocal
+  deploy
+  unlock
 }
 
 main
