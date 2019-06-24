2b6cf81dbdd8c04c8e1497703ba1e5ff7bf021bb
xinghunm <xinghunm@curacloudcorp.com>
2019-06-24 15:40:51 +0800

    delete redundant parentheses

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -7,29 +7,29 @@ DATE=$(date "+%Y-%m-%d-%H-%M")
 APP_NAME="front-end-ship-server"
 SCRIPT_NAME="deploy.sh"
 BASE_DIR="/tmp"
-LOCK_FILE="${BASE_DIR}/${APP_NAME}.lock"
-SHELL_LOG="${BASE_DIR}/${APP_NAME}.log"
+LOCK_FILE="$BASE_DIR/$APP_NAME.lock"
+SHELL_LOG="$BASE_DIR/$APP_NAME.log"
 REMOTE="root@140.82.48.232"
 SERVER_DIR_ON_REMOTE="/root/project/front-end-ship-server"
 
 
 lock(){
-  touch ${LOCK_FILE}
+  touch $LOCK_FILE
 }
 
 unlock(){
-  rm -f ${LOCK_FILE}
+  rm -f $LOCK_FILE
 }
 
 log(){
   LOG_INFO=$1
-  echo "$DATE ${SCRIPT_NAME} : ${LOG_INFO}" >> ${SHELL_LOG}
+  echo "$DATE $SCRIPT_NAME : $LOG_INF" >> $SHELL_LOG
 }
 
 pushDBJsonOnRemote(){
   printGreen "Push db.json on remote..."
   log "Push db.json on remote"
-  result=`ssh ${REMOTE} "cd $SERVER_DIR_ON_REMOTE && git add . && git commit -m 'update db.json' && git pull --rebase origin master && git push origin master"` || (unlock && unlock)
+  result=`ssh $REMOTE "cd $SERVER_DIR_ON_REMOTE && git add . && git commit -m 'update db.json' && git pull --rebase origin master && git push origin master"` || (unlock && unlock)
   echo $result
 }
 
@@ -48,7 +48,7 @@ updateDBJson(){
 deploy(){
   printGreen "Deploy Server..."
   log "Deploy Server"
-  ssh ${REMOTE} "cd /root/project/front-end-ship-server && git pull && pm2 stop all && yarn build && pm2 start server.js"
+  ssh $REMOTE "cd $SERVER_DIR_ON_REMOTE && git pull && pm2 stop all && yarn build && pm2 start server.js"
 }
 
 function printGreen(){
@@ -67,8 +67,8 @@ main(){
   fi
 
   if [ -f "$LOCK_FILE" ];then
-     log "${SCRIPT_NAME} is running"
-     printRed "${SCRIPT_NAME} is running" && exit
+     log "$SCRIPT_NAME is running"
+     printRed "$SCRIPT_NAME is running" && exit
   fi
   lock
   pushDBJsonOnRemote
